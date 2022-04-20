07225006cb3d57eb4da715a451e92261ffac8d75
Rafael Reis <rafa.araujoreis@gmail.com>
2022-04-19 22:27:45 -0300

    clear fields

--- src/app/new-transfer/new-transfer.component.ts
+++ src/app/new-transfer/new-transfer.component.ts
@@ -6,19 +6,31 @@ import { Component, EventEmitter, Output } from "@angular/core";
   styleUrls: ['./new-transfer.component.scss']
 })
 
-export class NewTransferComponent{
+export class NewTransferComponent {
 
-  @Output() handleTransfer = new EventEmitter<any>()
+  @Output() handleTransfer = new EventEmitter<any>();
 
   valueTransfer: number;
   countDestination: number;
+  active: boolean;
 
-  transferir(){
+  transferir() {
     const dataTransfer = {
       valueTransfer: this.valueTransfer,
       countDestination: this.countDestination
-    }
+    };
 
     this.handleTransfer.emit(dataTransfer);
+    this.clearFields();
+  }
+
+  clearFields() {
+    this.valueTransfer = 0;
+    this.countDestination = 0;
+  }
+
+  isValide($event) {
+    console.log($event !== null || $event > 0);
+    this.active = $event !== null || $event > 0;
   }
 }
